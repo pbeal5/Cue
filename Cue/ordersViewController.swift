5b8289db24babd4109e7d64646a8d94b2aa95cb7
Patrick Beal <patrickbeal@Patricks-MacBook-Pro.local>
2018-11-20 19:42:38 -0500

    Linked objects in view friends order view controllers. Next is to dive into backend.

--- Cue/ordersViewController.swift
+++ Cue/ordersViewController.swift
@@ -64,16 +64,6 @@ class ordersViewController: UIViewController {
         }
         
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     func fetchOrderItems(){
         let request: NSFetchRequest<Order> = Order.fetchRequest()
